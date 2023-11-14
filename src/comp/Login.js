8b1e99150c8c379e5fbdc2aed961e19283f57ac7
20SSJ <64567484+ssjn01@users.noreply.github.com>
2023-11-14 13:06:07 +0900

    커밋

--- src/comp/Login.js
+++ src/comp/Login.js
@@ -1,25 +1,22 @@
+import './CSS/Login.css';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
 const Login = (props) => {
     const { id, pwd, changeId, changePwd } = props;
     return (
         <>
-        <div></div>
-        <div id="login">
-            <h3 align="center">로그인</h3>
-            <table align="center" border="1">
-                <tr>
-                    <td>
-                        <input type="text" size="16" value={id} onChange={changeId} placeholder="아이디" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="password" size="16" value={pwd} onChange={changePwd} placeholder="비밀번호"/>
-                    </td>
-                </tr>
-            </table>
-            {id} {pwd} 
-        </div>
-        <div></div>
+            <main>
+                <div></div>
+                <div id="login">
+                    <h3>로그인</h3>
+                    <input type="text" size="16" value={id} onChange={changeId} placeholder="아이디" />
+                    <br />
+                    <input type="password" size="16" value={pwd} onChange={changePwd} placeholder="비밀번호" />
+                    <br /> 
+                </div>
+                <div></div>
+            </main>
         </>
     );
 };
