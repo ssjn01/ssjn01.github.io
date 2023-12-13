af9e07ff375692e87b8d56e7c774c40d9c125312
20SSJ <64567484+ssjn01@users.noreply.github.com>
2023-12-13 15:15:00 +0900

    commit

--- src/comp/Home.js
+++ src/comp/Home.js
@@ -5,26 +5,23 @@ import './CSS/Home.css';
 
 function Home() {
     return (
-      <>
-        <header>
-          <img src="/image/me.png" alt="나의 사진" id="profile" />
-          <span id="title"><span id="keyWord">"매일 발전하는 개발자"</span><br />
-            서상준입니다.</span>
-        </header>
-        <div id="content">
-          <div class="anima">
-            <div class="content-item">
-              <Intro />
-            </div>
-          </div>
-          <div></div>
-          <div class="anima">
-            <div class="content-item">
-              <Languages />
-            </div>
-          </div>
-        </div>
-      </>
+        <>
+            <header>
+                <img src="/image/me.png" alt="나의 사진" id="profile" />
+                <span id="title"><span id="keyWord">"매일 발전하는 개발자"</span><br />
+                    서상준입니다.</span>
+            </header>
+            <div id="content">
+                <div class="anima">
+                    <div class="content-item">
+                        <Intro />
+                    </div></div>
+                <div></div>
+                <div class="anima">
+                    <div class="content-item">
+                        <Languages />
+                    </div></div></div>
+        </>
     )
 };
 
